24c84bbd6569994bff6702d3b05c1958770a23da
Pragades Rajagopal <66718413+Pragades-Rajagopal@users.noreply.github.com>
2023-01-31 11:11:40 +0000

    Added users search with area & district

--- crowdsourcing/users/views.py
+++ crowdsourcing/users/views.py
@@ -4,21 +4,78 @@ from markets.models import Items, Categories, Markets
 
 
 def index(request):
-    return render(request, 'users/index.html')
+    return render(request, 'users/index.html', {"showtable": False})
 
 
 def searchitem(request):
     item_name = request.POST.get('itemname')
     district = request.POST.get('district')
     area = request.POST.get('area')
+    message = ""
 
     items = Items.objects.filter(item_name=item_name).order_by("price")
-    result = []
-    # for i in items:
-    #     c = Categories.objects.filter(pk=i.category_id)
-    #     m = Markets.objects.filter(id=c[0].market_id)
-    #     print(m[])
-    #     result.append(
-    #         {"market_name": m[0].market_name, "item": items[i]})
-    # print(result)
-    return render(request, 'users/searchitems.html', {"items": items})
+    results = []
+
+    if not district and not area:
+        if items:
+            for i in items:
+                c = Categories.objects.filter(pk=i.category_id)
+                m = Markets.objects.filter(id=c[0].market_id)
+
+                results.append({
+                    "item_id": i.id,
+                    "item_name": i.item_name,
+                    "price": i.price,
+                    "market_name": m[0].market_name,
+                    "district": m[0].district,
+                    "area": m[0].area,
+                    "updated_at": i.updated_at
+                })
+        else:
+            message = "Item currently unavailable!"
+
+    elif not area:
+        if items:
+            for i in items:
+                c = Categories.objects.filter(pk=i.category_id)
+                m = Markets.objects.filter(
+                    district=district, id=c[0].market_id)
+                if m:
+                    results.append({
+                        "item_id": i.id,
+                        "item_name": i.item_name,
+                        "price": i.price,
+                        "market_name": m[0].market_name,
+                        "district": m[0].district,
+                        "area": m[0].area,
+                        "updated_at": i.updated_at
+                    })
+                else:
+                    message = "Item unavailable in {}. Try searching without district!".format(
+                        district)
+        else:
+            message = "Item currently unavailable!"
+
+    else:
+        if items:
+            for i in items:
+                c = Categories.objects.filter(pk=i.category_id)
+                m = Markets.objects.filter(
+                    district=district, area=area, id=c[0].market_id)
+                if m:
+                    results.append({
+                        "item_id": i.id,
+                        "item_name": i.item_name,
+                        "price": i.price,
+                        "market_name": m[0].market_name,
+                        "district": m[0].district,
+                        "area": m[0].area,
+                        "updated_at": i.updated_at
+                    })
+                else:
+                    message = "Item unavailable in {0}, {1}. Try searching without area & district!".format(
+                        area, district)
+        else:
+            message = "Item currently unavailable!"
+
+    return render(request, 'users/index.html', {"results": results, "showtable": True, "message": message})
